a6d906904a52f74dae39106d44a5885035b44e14
next <next@>
2019-05-07 14:09:53 +0300

    finally commit

--- web_pools/modules/utils.py
+++ web_pools/modules/utils.py
@@ -39,5 +39,4 @@ async def check_password_hash(plain_password, password_hash):
     plain_password_bin = plain_password.encode('utf-8')
     password_hash_bin = password_hash.encode('utf-8')
     is_correct = bcrypt.checkpw(plain_password_bin, password_hash_bin)
-    print("password",plain_password, password_hash,is_correct)
     return is_correct
